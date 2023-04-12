98b06acf1fe8e48d5e48bbc1a4e500052b791cb3
Jason Kadman <jkadman231@gmail.com>
2023-04-12 12:12:22 +0000

    complete ex2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+@store2 = Store.second
+
+# @store1 = Store.find_by(name: 'Burnaby')
+# @store1.update(name: "Metrotown")
+@store1.update(name: "Metrotown")
+puts @store1.name
